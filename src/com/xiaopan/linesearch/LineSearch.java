08439c1c2661900075a7e0163dae02ba7d0deda0
xiaopantx <m15132582590@163.com>
2021-10-24 14:12:04 +0800

    abc

--- src/com/xiaopan/linesearch/LineSearch.java
+++ src/com/xiaopan/linesearch/LineSearch.java
@@ -6,43 +6,33 @@ public class LineSearch {
         // nothing to do
     }
 
-<<<<<<< HEAD
-=======
-    public static void main(String[] args) {
-        int[] data = {2, 11, 5, 52, 10, 100, 30};
-        int target = 3000;
-        int index = LineSearch.lineSearch(data, target);
-        if (index != -1)
-            System.out.println("index: " + index + ", value:" + data[index]);
-    }
-
->>>>>>> d956a03068740678acab1864ed91fd72e003b8ce
     /**
      * 线性查找
-     * @param arr 数组元素
+     * @param data 数组
      * @param target 目标
      * @return 索引值
      */
-<<<<<<< HEAD
-    public static int lineSearch(int[] arr, int target) {
-=======
-    private static int lineSearch(int[] arr, int target) {
->>>>>>> d956a03068740678acab1864ed91fd72e003b8ce
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
+    public static int lineSearch(int[] data, int target) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == target) {
                 return i;
             }
         }
         return -1;
     }
 
-<<<<<<< HEAD
+    /**
+     * 泛型线性查找支持所有引用数据类型
+     * @param data 数组
+     * @param target 目标
+     * @return 索引值
+     */
     public static <E extends Comparable<E>> int lineSearch(E[] data, E target) {
-=======
-    private static <E extends Comparable<E>> int lineSearch(E[] data, E target) {
->>>>>>> d956a03068740678acab1864ed91fd72e003b8ce
-
-
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].compareTo(target) == 0) {
+                return i;
+            }
+        }
         return -1;
     }
 
