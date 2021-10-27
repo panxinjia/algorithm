70adf8b876e80eb2e72a98dfa5ca3352cbfe5a06
xiaopantx <m15132582590@163.com>
2021-10-27 18:48:37 +0800

    冒泡排序算法

--- sort/src/com/xiaopan/sort/BubbleSort.java
+++ sort/src/com/xiaopan/sort/BubbleSort.java
@@ -1,5 +1,11 @@
 package com.xiaopan.sort;
 
+import com.xiaopan.sort.tools.ArrayGenerator;
+import com.xiaopan.sort.tools.SortHelper;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 /**
  * 冒泡排序算法
  */
@@ -10,6 +16,25 @@ public class BubbleSort {
     }
 
     public static <E extends Comparable<E>> void genericSort(E[] data) {
-        //TODO 冒泡排序
+        if (data == null || data.length < 2) {
+            return;
+        }
+        for (int i = 0; i < data.length; i++) {
+            for(int j = 0; j < data.length - i -1; j++) {
+                if (data[j].compareTo(data[j + 1]) > 0) {
+                    SortHelper.swap(data, j, j + 1);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Integer[] data = ArrayGenerator.generatorRandomArray(10, 100);
+        System.out.println(Arrays.toString(data));
+        genericSort(data);
+        System.out.println(Arrays.toString(data));
+
+
     }
 }
